ca9bf11756f347f72814e1150229107cf0a0153f
Jackeybai <1303802862@qq.com>
2022-10-06 12:34:38 +0800

    创建欢迎界面，配置路由完成

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { history } from './shared/history';
 import { createApp } from "vue";
 import { App } from "./App";
-import { Bar } from "./views/Bar";
-import { Foo } from "./views/Foo";
 import { createRouter } from "vue-router";
 import { routes } from "./config/routes";
 
