55a63a13b8547069e20841f5baee7673d6443719
Jackeybai <1303802862@qq.com>
2022-10-06 12:22:23 +0800

    重构 main.tsx

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
+import { history } from './shared/history';
 import { createApp } from "vue";
 import { App } from "./App";
 import { Bar } from "./views/Bar";
 import { Foo } from "./views/Foo";
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter } from "vue-router";
+import { routes } from "./config/routes";
 
-const routes = [
-  { path: "/", component: Foo },
-  { path: "/bar", component: Bar },
-];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history,
   routes,
 });
 
