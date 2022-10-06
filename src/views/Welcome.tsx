4e07aec01d56494a61bd344e4f59002d1a0cd2fc
Jackeybai <1303802862@qq.com>
2022-10-06 14:44:27 +0800

    css 初始化

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,9 +1,20 @@
-import { defineComponent } from 'vue';
-import { RouterView } from 'vue-router';
+import { defineComponent } from "vue";
+import { RouterView } from "vue-router";
+import s from "./Welcome.module.scss";
+import logo from "../assets/svg/logo.svg";
+
 export const Welcome = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div><RouterView/></div>
-    )
-  }
-})
+  setup: (props, context) => {
+    return () => (
+      <div class={s.wrapper}>
+        <header>
+          <img src={logo} />
+          <h1>星空记账</h1>
+        </header>
+        <main>
+          <RouterView />
+        </main>
+      </div>
+    );
+  },
+});
