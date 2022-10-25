386a1ca20251c6e87269dd6ac705aa1b029978f3
Jackeybai <1303802862@qq.com>
2022-10-25 15:15:51 +0800

    添加useSwipe

---
+++ src/hooks/useSwipe.tsx
@@ -0,0 +1,80 @@
+import { computed, onMounted, onUnmounted, ref, Ref } from "vue";
+
+type Point = { x: number; y: number };
+
+export const useSwipe = (element: Ref<HTMLElement | null>) => {
+  const start = ref<Point>();
+  const end = ref<Point>();
+  const swiping = ref(false);
+  const distance = computed(() => {
+    // 算出移动的距离
+    if (!end.value || !start.value) {
+      return undefined;
+    }
+    return {
+      x: end.value.x - start.value.x,
+      y: end.value.y - start.value.y,
+    };
+  });
+  const direction = computed(() => {
+    if (!swiping) {
+      return "";
+    }
+    if (!distance.value) {
+      return "";
+    }
+    const { x, y } = distance.value;
+    if (Math.abs(x) > Math.abs(y)) {
+      // 比较绝对值，是往左右还是上下，如果x绝对值比y大说明是左右滑动，反之亦然
+      return x > 0 ? "right" : "left";
+    } else {
+      return y > 0 ? "down" : "up";
+    }
+  });
+  const onStart = (e: TouchEvent) => {
+    start.value = {
+      x: e.touches[0].clientX,
+      y: e.touches[0].clientY,
+    };
+    end.value = undefined;
+    swiping.value = true;
+  };
+  const onMove = (e: TouchEvent) => {
+    end.value = {
+      x: e.touches[0].clientX,
+      y: e.touches[0].clientY,
+    };
+  };
+  const onEnd = (e: TouchEvent) => {
+    swiping.value = false;
+    start.value = undefined;
+    end.value = undefined;
+  };
+  onMounted(() => {
+    if (!element.value) {
+      return;
+    }
+    element.value?.addEventListener("touchstart", onStart);
+    element.value?.addEventListener("touchmove", onMove);
+    element.value?.addEventListener("touchend", onEnd);
+  });
+   //onMounted 是Vue官方的一个钩子 一旦组件被挂载就去监听元素的touchstart事件
+  onUnmounted(() => {
+    if (!element.value) {
+      return;
+    }
+    element.value?.removeEventListener("touchstart", onStart);
+    element.value?.removeEventListener("touchmove", onMove);
+    element.value?.removeEventListener("touchend", onEnd);
+  });
+  // 用onUnmounted把污染的元素恢复
+  return {
+    swiping,
+    distance,
+    direction,
+    start,
+    end,
+  };
+
+ 
+};
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,17 @@
-import { defineComponent, h, Transition, VNode } from "vue";
+import { defineComponent, h, ref, Transition, VNode, watchEffect } from "vue";
 import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
 import s from "./Welcome.module.scss";
 import logo from "../assets/icons/logo.svg";
+import { useSwipe } from "../hooks/useSwipe";
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
+    const main = ref<HTMLElement | null>(null);
+    const { direction, swiping } = useSwipe(main);
+    watchEffect(() => {
+      console.log(swiping.value, direction.value);
+    });
+    // watchEffect 类似于React的 useEffect
     return () => (
       <div class={s.wrapper}>
         <header>
@@ -13,7 +20,7 @@ export const Welcome = defineComponent({
           </svg>
           <h1>星空记账</h1>
         </header>
-        <main class={s.main}>
+        <main class={s.main} ref={main}>
           <RouterView name="main">
             {({
               Component: X,
