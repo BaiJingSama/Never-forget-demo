309541b8f7dc6e7743b58f05424462ed52e0ed3a
Jackeybai <1303802862@qq.com>
2022-10-31 20:27:47 +0800

    给date和amount加样式

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -2,6 +2,7 @@ import { defineComponent, PropType, ref } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
 import { Tab, Tabs } from "../../shared/Tags";
+import { InputPad } from "./InputPad";
 import s from "./ItemCreate.module.scss";
 export const ItemCreate = defineComponent({
   props: {
@@ -22,11 +23,13 @@ export const ItemCreate = defineComponent({
                 selected={refKind.value}
                 onUpdateSelected={(name: string) => (refKind.value = name)}
               > */}
-                <Tabs v-model:selected={refKind.value}>
+              <Tabs v-model:selected={refKind.value}>
                 <Tab name="支出">icon 列表</Tab>
                 <Tab name="收入">icon 列表2</Tab>
               </Tabs>
-              <div></div>
+              <div class={s.inputPad_wrapper}>
+                <InputPad />
+              </div>
             </>
           ),
         }}
