ae3c6940569339731cb75b5c5c52b15391d7f99c
Jackeybai <1303802862@qq.com>
2022-11-02 16:14:21 +0800

    完成创建标签表单功能

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -4,6 +4,7 @@ import { Icon } from "../../shared/Icon";
 import { Button } from "../../shared/Button";
 import s from "./TagCreate.module.scss";
 import { EmojiSelect } from "../../shared/EmojiSelect";
+import { Rules, validate } from "../../shared/validate";
 export const TagCreate = defineComponent({
   props: {
     modelValue: {
@@ -11,19 +12,27 @@ export const TagCreate = defineComponent({
     },
   },
   setup: (props, context) => {
-    const formDate = reactive({
+    const formData = reactive({
       name: "",
       sign: "",
     });
+    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({});
     const onSubmit = (e: Event) => {
-      console.log(toRaw(formDate));
-
-      // const rules = [
-      //   { key: "name", required: true, message: "必填" },
-      //   { key: "name", pattern: /^.{1,4}$/, message: "必填" },
-      //   { key: "sign", required: true },
-      // ];
-      // const errors = validate(formDate, rules);
+      const rules: Rules<typeof formData> = [
+        { key: "name", type: "required", message: "必填" },
+        {
+          key: "name",
+          type: "pattern",
+          regex: /^.{1,4}$/,
+          message: "只能填 1-4 个字符",
+        },
+        { key: "sign", type: "required", message: "必填" },
+      ];
+      Object.assign(errors, {
+        name: undefined,
+        sign: undefined,
+      });
+      Object.assign(errors, validate(formData, rules));
       e.preventDefault();
     };
     return () => (
@@ -39,27 +48,39 @@ export const TagCreate = defineComponent({
                     <span class={s.formItem_name}>标签名</span>
                     <div class={s.formItem_value}>
                       <input
-                        v-model={formDate.name}
+                        v-model={formData.name}
                         class={[s.formItem, s.input, s.error]}
                       ></input>
                     </div>
                     <div class={s.formItem_errorHint}>
-                      <span>必填</span>
+                      <span>
+                        {errors["name"] ? (
+                          errors["name"]?.[0]
+                        ) : (
+                         '　'
+                        )}
+                      </span>
                     </div>
                   </label>
                 </div>
 
                 <div class={s.formRow}>
                   <label class={s.formLabel}>
-                    <span class={s.formItem_name}>符号 {formDate.sign}</span>
+                    <span class={s.formItem_name}>符号 {formData.sign}</span>
                     <div class={s.formItem_value}>
                       <EmojiSelect
-                        v-model={formDate.sign}
+                        v-model={formData.sign}
                         class={[s.formItem, s.emojiList, s.error]}
                       />
                     </div>
                     <div class={s.formItem_errorHint}>
-                      <span>必填</span>
+                      <span>
+                        {errors["sign"] ? (
+                          errors["sign"]?.[0]
+                        ) : (
+                          '　'
+                        )}
+                      </span>
                     </div>
                   </label>
                 </div>
