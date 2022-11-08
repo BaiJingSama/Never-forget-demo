a6b1fb395b0b47841edc5501852dadfe9d8d3728
Jackeybai <1303802862@qq.com>
2022-11-08 16:40:09 +0800

    登录成功后保存JWT到localStorage

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -4,6 +4,7 @@ import { useBool } from "../hooks/useBool";
 import { MainLayout } from "../layouts/MainLayout";
 import { Button } from "../shared/Button";
 import { Form, FormItem } from "../shared/Form";
+import { history } from "../shared/history";
 import { http, HttpClient } from "../shared/HttpClient";
 import { Icon } from "../shared/Icon";
 import { validate, hasError } from "../shared/validate";
@@ -50,7 +51,9 @@ export const SignInPage = defineComponent({
         ])
       );
       if (!hasError(errors)) {
-        const response = await http.post("/session", formData);
+        const response = await http.post<{ jwt: string }>("/session", formData);
+        localStorage.setItem("jwt", response.data.jwt);
+        history.push("/");
       }
     };
     const onError = (error: any) => {
