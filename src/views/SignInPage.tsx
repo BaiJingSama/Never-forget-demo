474bed87218d565d152050b3ef952ad72b01698f
Jackeybai <1303802862@qq.com>
2022-11-09 16:04:15 +0800

    删除测试代码

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -56,9 +56,7 @@ export const SignInPage = defineComponent({
       );
       if (!hasError(errors)) {
         const response = await http
-          .post<{ jwt: string }>("/session", formData, {
-            params: { _mock: "session" },
-          })
+          .post<{ jwt: string }>("/session", formData)
           .catch(onError);
         localStorage.setItem("jwt", response.data.jwt);
         //下方代码为保存到查询参数里
