4b1206b8d858966d2130f45767033e2a4ce9dc52
Jackeybai <1303802862@qq.com>
2022-11-22 14:09:01 +0800

    添加腾讯云COS上传工具

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # 我的 Vue3 + TSX 项目
 
+## 部署
+
+```bash
+pnpm run build --base /
+bin/coscli-windows cp -r dist cos://starryskytally-test-1-1315213295
+```
+
 ## 如何开发
 
 ## 如何打包
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import styleImport, { VantResolve } from "vite-plugin-style-import";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
 // @ts-nocheck
-import { svgstore } from "./src/vite_plugins/svgstore";
+import { svgstore } from './src/vite_plugins/svgstore'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,10 +20,10 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      "/api/v1": {
-        target: "http://121.196.236.94:3000",
+      '/api/v1': {
+        target: 'http://121.196.236.94:3000',
         // target: "http://121.196.236.94",
       },
     },
   },
-});
+})
